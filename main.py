3a75cdfef5c77bffa531cefbb4bc640ebe9ed715
devenderbutani21 <devenderbutani21@gmail.com>
2020-08-19 12:03:51 +0530

    Basic Changes to API

--- main.py
+++ main.py
@@ -39,15 +39,181 @@ def armstrong(n):
 @app.route('/myData')
 def myData():
     result = {
-        "firstName": "Delta",
-        "lastName": "Alpha",
-        "age": 27,
-        "address": {
-            "streetAddress": "Plot-8, Raja Nagar",
-            "city": "New Delhi",
-            "state": "Delhi",
-            "postalCode": "110059"
-        }
+        "1": {
+            "category": "Pizza",
+            "items": "25",
+            "img_url": "https://cdn.pixabay.com/photo/2017/12/05/20/10/pizza-3000285_1280.png",
+            "menu": {
+                "1": {
+                    "name": "Pepperoni",
+                    "price_in_dollar": "7.0",
+                    "rating": "4",
+                    "portion_size": "200g",
+                    "img_url": "",
+                },
+                "2": {
+                    "name": "Mushroom",
+                    "price_in_dollar": "10.0",
+                    "rating": "3",
+                    "portion_size": "200g",
+                    "img_url": "",
+                },
+                "3": {
+                    "name": "Mexican Street Corn",
+                    "price_in_dollar": "12.0",
+                    "rating": "4",
+                    "portion_size": "200g",
+                    "img_url": "",
+                },
+                "4": {
+                    "name": "Buffalo Chicken",
+                    "price_in_dollar": "14.0",
+                    "rating": "4",
+                    "portion_size": "200g",
+                    "img_url": "",
+                },
+            },
+        },
+        "2": {
+            "category": "Salads",
+            "items": "40",
+            "img_url": "https://cdn.pixabay.com/photo/2016/12/05/10/07/dish-1883501_1280.png",
+            "menu": {
+                "1": {
+                    "name": "Summer Asian Slaw",
+                    "price_in_dollar": "8.0",
+                    "rating": "4",
+                    "portion_size": "250g",
+                    "img_url": "",
+                },
+                "2": {
+                    "name": "Broccoli",
+                    "price_in_dollar": "10.0",
+                    "rating": "3",
+                    "portion_size": "300g",
+                    "img_url": "",
+                },
+                "3": {
+                    "name": "Shredded Brussels Sprout",
+                    "price_in_dollar": "12.0",
+                    "rating": "4",
+                    "portion_size": "300g",
+                    "img_url": "",
+                },
+                "4": {
+                    "name": "Pasta Salad",
+                    "price_in_dollar": "7.0",
+                    "rating": "3",
+                    "portion_size": "250g",
+                    "img_url": "",
+                },
+            },
+        },
+        "3": {
+            "category": "Desserts",
+            "items": "30",
+            "img_url": "https://cdn.pixabay.com/photo/2016/04/04/09/09/dessert-1306397_1280.png",
+            "menu": {
+                "1": {
+                    "name": "Fried Ice Cream",
+                    "price_in_dollar": "10.0",
+                    "rating": "4",
+                    "portion_size": "100g",
+                    "img_url": "",
+                },
+                "2": {
+                    "name": "Samoa Dessert Lasagna",
+                    "price_in_dollar": "8.0",
+                    "rating": "3",
+                    "portion_size": "100g",
+                    "img_url": "",
+                },
+                "3": {
+                    "name": "Oreo Truffles",
+                    "price_in_dollar": "12.0",
+                    "rating": "4",
+                    "portion_size": "100g",
+                    "img_url": "",
+                },
+                "4": {
+                    "name": "Chocolate Pudding",
+                    "price_in_dollar": "7.0",
+                    "rating": "3",
+                    "portion_size": "100g",
+                    "img_url": "",
+                },
+            },
+        },
+        "4": {
+            "category": "Pasta",
+            "items": "44",
+            "img_url": "https://cdn.pixabay.com/photo/2017/04/08/06/27/penne-2212559_1280.png",
+            "menu": {
+                "1": {
+                    "name": "Pesto",
+                    "price_in_dollar": "8.0",
+                    "rating": "4",
+                    "portion_size": "250g",
+                    "img_url": "",
+                },
+                "2": {
+                    "name": "Lemon Linguine with Roasted Tomatoes",
+                    "price_in_dollar": "10.0",
+                    "rating": "3",
+                    "portion_size": "300g",
+                    "img_url": "",
+                },
+                "3": {
+                    "name": "Fettuccine Alfredo",
+                    "price_in_dollar": "12.0",
+                    "rating": "4",
+                    "portion_size": "250g",
+                    "img_url": "",
+                },
+                "4": {
+                    "name": "Spaghetti Bolognese",
+                    "price_in_dollar": "10.0",
+                    "rating": "3",
+                    "portion_size": "250g",
+                    "img_url": "",
+                },
+            },
+        },
+        "5": {
+            "category": "Beverages",
+            "items": "30",
+            "img_url": "https://cdn.pixabay.com/photo/2018/05/12/19/56/coffee-cup-3394435_1280.png",
+            "menu": {
+                "1": {
+                    "name": "Hot Chocolate Frost",
+                    "price_in_dollar": "2.0",
+                    "rating": "4",
+                    "portion_size": "50g",
+                    "img_url": "",
+                },
+                "2": {
+                    "name": "Peppermint Mocha flavor",
+                    "price_in_dollar": "3.0",
+                    "rating": "3",
+                    "portion_size": "50g",
+                    "img_url": "",
+                },
+                "3": {
+                    "name": "Caramel Brulee Latte",
+                    "price_in_dollar": "4.0",
+                    "rating": "4",
+                    "portion_size": "50g",
+                    "img_url": "",
+                },
+                "4": {
+                    "name": "Winter Wonderland",
+                    "price_in_dollar": "3.5",
+                    "rating": "3",
+                    "portion_size": "50g",
+                    "img_url": "",
+                },
+            },
+        },
     }
     return jsonify(result)
 
